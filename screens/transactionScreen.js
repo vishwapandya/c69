fafe428ae29371cee9319be95d8875b168be2125
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-03-26 12:26:45 +0530

    Update transactionScreen.js

--- screens/transactionScreen.js
+++ screens/transactionScreen.js
@@ -1,12 +1,83 @@
 import React from 'react';
-import {Text,View} from 'react-native';
+import {Text,View,StyleSheet,TouchableOpacity} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
 export default class TransactionScreen extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            hasCameraPermissions: null,
+            scanned: false,
+            scannedData: '',
+            buttonState: 'normal'
+        }
+    }
+
+    getCameraPermissions = async() =>{
+        const {status} = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+            /*status === 'granted' is true when user has granted permission 
+            status === 'granted' is false when user has denied permission*/
+            hasCameraPermissions: status === 'granted',
+            buttonState: 'clicked',
+            scanned: false
+        })
+    }
+    
+    handleBarCodeScanned = async({type,data})=>{
+        this.setState({
+            scanned: true,
+            scannedData: data,
+            buttonState: 'normal'
+        })
+    }
+
     render(){
+        const hasCameraPermissions = this.state.hasCameraPermissions;
+        const scanned = this.state.scanned;
+        const buttonState = this.state.buttonState;
+        if(buttonState === 'clicked' && hasCameraPermissions){
+            return(
+                <BarCodeScanner
+                onBarCodeScanned = {scanned ? undefined : this.handleBarCodeScanned}
+                style = {StyleSheet.absoluteFillObject}
+                />
+            )
+        }
+        else if(buttonState === 'normal'){
         return(
-            <View style={{justifyContent:'center',alignItems:'center'}}>
-                <Text>Issue Or Return?</Text>
+            <View style={styles.container}>
+                <Text style={styles.displayText}>
+                    {hasCameraPermissions === true ? this.state.scannedData:"Request Camera Permission"}
+                </Text>
+                <TouchableOpacity onPress={this.getCameraPermissions} style={styles.scanButton}>
+                    <Text style={styles.buttonText}>Scan QR Code</Text>
+                </TouchableOpacity>
             </View>
         )
+        }
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    displayText:{
+        fontSize: 15,
+        textDecorationLine: 'underline'
+    },
+    scanButton:{
+        backgroundColor: '#1A91A9',
+        padding: 10,
+        margin: 10
+    },
+    buttonText:{
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
